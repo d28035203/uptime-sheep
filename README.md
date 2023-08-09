66c6c02f3a50f47625acd3c029de21234360fbfa
d28035203 <289484834+d28035203@users.noreply.github.com>
2023-08-09 12:54:26 +0530

    feat: configurable interval health checks

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # Uptime Sheep
 
-**HTTP uptime checker** with retries and JSON config.
+HTTP uptime checker: JSON target list, retries, timeouts, one-shot or loop mode.
 
-```bash
-python3 sheep.py --once
+## Config (`flock.json`)
+
+```json
+{
+  "timeout_sec": 5,
+  "retries": 2,
+  "targets": [
+    {"name": "example", "url": "https://example.com"},
+    {"name": "httpbin", "url": "https://httpbin.org/status/200"}
+  ]
+}
 ```
 
-Reliability practice · ridiculous name.
+## Usage
 
-MIT · 2023
+```bash
+python3 sheep.py --once          # exit 1 if any target is down
+python3 sheep.py --interval 60   # loop every 60s
+```
+
+## License
+
+MIT
--- sheep.py
+++ sheep.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
-"""uptime-sheep — count HTTP sheep (health checks)."""
+"""uptime-sheep — HTTP health checks with retries and JSON config."""
 from __future__ import print_function
-import argparse, json, os, sys, time, urllib.request
+
+import argparse
+import json
+import os
+import sys
+import time
+import urllib.request
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+
 
 def check(url, timeout, retries):
     last_err = None
@@ -15,37 +22,43 @@ def check(url, timeout, retries):
                 if 200 <= code < 400:
                     return True, code, None
                 last_err = "HTTP %s" % code
-        except Exception as e:
-            last_err = str(e)
+        except Exception as exc:
+            last_err = str(exc)
         time.sleep(0.4 * (attempt + 1))
     return False, None, last_err
 
+
 def main():
-    p = argparse.ArgumentParser()
-    p.add_argument("--once", action="store_true")
-    p.add_argument("--config", default=os.path.join(HERE, "flock.json"))
-    args = p.parse_args()
-    cfg = json.load(open(args.config))
+    parser = argparse.ArgumentParser(description="HTTP uptime checker")
+    parser.add_argument("--once", action="store_true", help="single pass, exit 1 if any target fails")
+    parser.add_argument("--config", default=os.path.join(HERE, "flock.json"))
+    parser.add_argument("--interval", type=int, default=30, help="seconds between passes (loop mode)")
+    args = parser.parse_args()
+
+    with open(args.config) as fh:
+        cfg = json.load(fh)
     timeout = cfg.get("timeout_sec", 5)
     retries = cfg.get("retries", 1)
 
     def run_round():
-        bad = 0
-        print("=== uptime sheep %s ===" % time.strftime("%H:%M:%S"))
-        for t in cfg["targets"]:
-            ok, code, err = check(t["url"], timeout, retries)
+        failed = 0
+        print("=== check %s ===" % time.strftime("%Y-%m-%d %H:%M:%S"))
+        for target in cfg["targets"]:
+            ok, code, err = check(target["url"], timeout, retries)
             if ok:
-                print("[awake] %-12s %s" % (t["name"], code))
+                print("[up]   %-16s %s" % (target["name"], code))
             else:
-                bad += 1
-                print("[asleep] %-12s %s" % (t["name"], err))
-        return bad
+                failed += 1
+                print("[down] %-16s %s" % (target["name"], err))
+        return failed
 
     if args.once:
         return 1 if run_round() else 0
+
     while True:
         run_round()
-        time.sleep(30)
+        time.sleep(args.interval)
+
 
 if __name__ == "__main__":
     sys.exit(main() or 0)
